3977aecd50e5707918cb835ebc66cff85c358c7c
Sam Caldwell <mail@samcaldwell.net>
2014-10-27 19:31:23 -0500

    adding article content.

--- routerClass.py
+++ routerClass.py
@@ -19,11 +19,17 @@ class RouterClass:
             articleList+="<div id='articleListRow'>{}</div>".format(article)
         with open('./templates/home.html','r') as f:
             html=f.read()
-            return html.('ARTICLE_LIST_HTML',articleList)
-            
-        
-
-
+            return html.replace('ARTICLE_LIST_HTML',articleList)
 
     def show_page(pageName):
-        #
+        with open('./templates/article.html') as t:
+            html=t.read()
+            articleFilename='./articles/{}'.format(pageName)
+            with open(articleFilename,'r') as f:
+                html=html.replace('articleTitle',articleTitle)
+                         .replace('articleAuthor',articleAuthor)
+                         .replace('articleKeywords',articleKeywords)
+                         .replace('articleDescription',articleDescription)
+                         .replace('articleCopyright',articleCopyright)
+                         .replace('articleFooter',articleFooter)
+                         .replace('articleContent',articleContent)
